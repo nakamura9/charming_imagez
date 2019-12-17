bf02d2072f26a7dbceb898c3d1f439e1701c8222
caleb kandoro <kandoroc@hotmail.com>
2019-12-17 17:37:44 +0200

    initial

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Comp = () =>{
+    return(<h1>Testing</h1>)
+}
+
+ReactDOM.render(<Comp />, document.getElementById('root'))
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+
+    context: __dirname,
+    entry:  './src/index.js',
+    output: {
+        path: path.resolve('./bundles/'),
+        filename: '[name].js',
+    },
+    
+    mode: 'development',
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                'loader': 'style-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'css-loader',
+                query: {
+                    modules: true,
+                    localIdentName: '[name]__[local]__[hash:64:5]'
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['stage-2','react']//stage 2 for class level attrs and autobind
+                }
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: [ '.js', '.jsx']
+    }
+}
