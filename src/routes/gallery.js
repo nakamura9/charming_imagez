aa12356c9aa78a1e0fa98391d8698fb9e2935c02
caleb kandoro <kandoroc@hotmail.com>
2019-12-29 22:41:09 +0200

    gallery

--- src/routes/gallery.js
+++ src/routes/gallery.js
@@ -1,16 +1,107 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react'
 import PolaroidWidget from '../components/polaroid'
+import PhotoGallery from 'react-photo-gallery'
+import Carousel, { Modal, ModalGateway } from "react-images"
+
+const photos = [
+    {
+        src: "/static/images/mombe.PNG",
+        width: 1,
+        height: 1
+    },
+    {
+        src: "/static/images/stairs.PNG",
+        width: 1,
+        height: 1
+    },
+    {
+        src: "/static/images/car.PNG",
+        width: 1,
+        height: 1
+    },
+    {
+        src: "/static/images/proposal.PNG",
+        width: 1,
+        height: 1
+    },
+    {
+        src: "/static/images/pregnant.PNG",
+        width: 1,
+        height: 1
+    },
+    {
+        src: "/static/images/carousel.PNG",
+        width: 1.5,
+        height: 1
+    },
+    {
+        src: "/static/images/rapids.PNG",
+        width: 2,
+        height: 1
+    },
+    {
+        src: "/static/images/tsvimbo.PNG",
+        width: 1,
+        height: 2
+    },
+    {
+        src: "/static/images/cuddle.PNG",
+        width: 1,
+        height: 1
+    }
+]
+
 const Gallary =(props) =>{
+    const [currentImage, setCurrentImage] = useState(0)
+    const [viewerIsOpen, setViewerIsOpen] = useState(false)
+
+    const openLightbox = useCallback((event, { photo, index }) => {
+        setCurrentImage(index);
+        setViewerIsOpen(true);
+    }, [])
+
+    const closeLightbox = () => {
+        setCurrentImage(0);
+        setViewerIsOpen(false);
+    };
     return(
-        <div className="polaroid-container shadow">
-            <div id='widget-container'>
-                <PolaroidWidget images={["/static/images/carousel.PNG", "/static/images/me.jpg", "/static/images/logo.png"]} />
-            </div>
-            <div id='about-text'>
-                <h3>Record the whole experience</h3>
-                <p>From first glance to gentle serenade</p>
+        <React.Fragment>
+            <div className="container-fluid">
+            {/*<div className="row">
+                <div className="col-lg-6 col-md-12 polaroid-container">
+                    <PolaroidWidget images={[
+                        "/static/images/carousel.PNG", 
+                        "/static/images/me.jpg", 
+                        "/static/images/logo.png"]} />
+                </div>
+                <div className="col-lg-6 col-md-12">
+                    <div id='about-text'>
+                        <h3>Record the whole experience</h3>
+                        <p>From first glance to gentle serenade</p>
+                    </div>
+                </div>
+            </div>*/}
+            <div className="row">
+                <div className="col-12">
+                    <PhotoGallery photos={photos} onClick={openLightbox}/>
+                </div>
             </div>
         </div>
+        <ModalGateway>
+            {viewerIsOpen ? (
+                <Modal onClose={closeLightbox}>
+                <Carousel
+                    currentIndex={currentImage}
+                    views={photos.map(x => ({
+                    ...x,
+                    srcset: x.srcSet,
+                    caption: x.title
+                    }))}
+                />
+                </Modal>
+            ) : null}
+        </ModalGateway>
+        </React.Fragment>
     )
 }
 
--- src/routes/home.js
+++ src/routes/home.js
@@ -38,25 +38,49 @@ return(
             text='We can execute your creative vision'
         />
         <RevealCard 
-            src='/static/images/carousel.PNG'
-            heading='Traditional Things!'
-            text='We can execute your creative vision'
+            src='/static/images/proposal.PNG'
+            heading="Will She? Won't she?!"
+            text='Capture the heart-pounding moments before a life changing decision.'
         />
         <RevealCard 
-            src='/static/images/carousel.PNG'
-            heading='Traditional Things!'
-            text='We can execute your creative vision'
+            src='/static/images/pregnant.PNG'
+            heading="There's a bun in the oven!"
+            text='As your family begins to grow...'
         />
     </div>
     
     </div>
-    
-    <div className="jumbotron" style={{
+    <div className="jumbotron " style={{
         color: '#ffffee',
         background: 'linear-gradient(to left, rgb(184,59,94), rgb(255,255,238))',marginBottomL:'0px'
     }}>
         <h1 style={{'fontFamily': 'AlexBrush', fontSize: '5rem', fontWeight: '200', textAlign: 'right'}}>Testimonials</h1>
-        <Carousel>
+        
+        <div className='testimonial__container-flex'>
+        <TestimonialCard 
+            img='/static/images/rumbi-testimonial.png'
+            text='A charming beginning with potential for massive growth...'
+            name='Rumbidzai'
+            location='Harare'/>
+        <TestimonialCard 
+            img='/static/images/tinsel-testimonial.png'
+            text="I've had shoots before, but this guy really knows his craft.... I would recommend him any day, any place. Absolutely the best in the industry."
+            name='Tinsel'
+            location='Harare'/>
+        <TestimonialCard 
+            img='/static/images/anonymous.jpg'
+            text="Had a photoshoot with Gerald recently, and was very impressed with his professionalism and skill. He is amazing with kids and made the experience enjoyable for the whole family. The photos are amazing and Gerald created everlasting memories for my family."
+            name='Sandy'
+            location='Harare'/>
+        <TestimonialCard 
+            img='/static/images/valery-testimonial.png'
+            text="They focus on the details. They are the very best!"
+            name='Valery'
+            location='Harare'/>
+
+    </div>
+ 
+        <Carousel className='testimonial__container-carousel'>
         <Carousel.Item>
             <TestimonialCard 
                 img='/static/images/rumbi-testimonial.png'
